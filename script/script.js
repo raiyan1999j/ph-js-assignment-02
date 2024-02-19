f75e5c732a36545b2b69ca9b5762a4635f0fe3d1
raiyan1999j <62192284+raiyan1999j@users.noreply.github.com>
2024-02-19 10:54:28 +0600

    upgade the privious one

--- script/script.js
+++ script/script.js
@@ -5,27 +5,49 @@ const totalSeat = Number(document.getElementById('remainSeat').innerText);
 for(let repeat of selectedSeat){
     repeat.addEventListener('click',(event)=>{
         const takeSeat = event.srcElement.innerText;
-        let holder ='';
-    
+        const selectedBtn = event.srcElement.classList;
+
         if(!seatArray.includes(takeSeat)){
             seatArray.push(takeSeat)
+            toggleSelection(selectedBtn,'add','bg-sky-500')
         }else{
             seatArray = seatArray.filter((value)=>{
                 return value != takeSeat
             })
+            toggleSelection(selectedBtn,'remove','bg-sky-500')
         }
 
-        limitedSeat(seatArray)
+        limitedSeat(seatArray,selectedBtn)
     })
 }
 
-function limitedSeat(confirmation){
+function limitedSeat(confirmation,event){
     let remainSeat = document.getElementById('remainSeat');
+    let seatAlert = document.getElementById('seatAlert');
     let confirmLen =Number(confirmation.length);
 
     if(confirmLen > 4){
         confirmation.splice(confirmLen - 1,1);
+        seatAlert.classList.remove('hidden');
+        setTimeout(()=>{
+            seatAlert.classList.add('hidden')
+        },2000)
+        toggleSelection(event,'remove','bg-sky-500')
     }else{
         remainSeat.innerText = totalSeat - confirmLen;
     }
+
+    totalSeatPrice(confirmation)
+}
+
+function toggleSelection(event,option,value){
+    if(option == 'add'){
+        event.add(value,'text-white')
+    }else{
+        event.remove(value,'text-white')
+    }
+}
+
+function totalSeatPrice(listedSeat){
+    console.log(listedSeat)
 }
